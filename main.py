79cc2661dbfe678476e4c83ac07ddde35aeef33b
Ganesh <ganuofficial2795@gmail.com>
2021-04-29 18:26:03 +0530

    function completed

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # import required module
 import winsound
 
+def Beep_sound():
+    """This function is used to generate beep sound """
 
+    frequency ,duration = 2500 ,5000
+    return winsound.Beep(frequency,duration)
 
-
+    
 if __name__ == '__main__':
 
+    Beep_sound()
     print("Code Completed")
